3c84f8e5d68df95973944e507e69ad4e51905fb3
Mingxun Wang <mwang87@gmail.com>
2022-07-11 19:10:00 -0700

    adding more dtabase standards

---
+++ workflow/bin/get_database.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/biorack/metatlas-data/main/C18/C18_standards_negative.tsv
+wget https://raw.githubusercontent.com/biorack/metatlas-data/main/C18/C18_standards_positive.tsv
+wget https://raw.githubusercontent.com/biorack/metatlas-data/main/HILIC/HILIC_standards_positive.tsv
+wget https://raw.githubusercontent.com/biorack/metatlas-data/main/HILIC/HILIC_standards_negative.tsv
